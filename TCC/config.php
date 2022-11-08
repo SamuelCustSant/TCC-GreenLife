208f9a88df278a0ec9ee6e960e9f9dafc80c6105
SamuelCustSant <105070591+SamuelCustSant@users.noreply.github.com>
2022-11-08 12:23:04 -0300

    Delete TCC directory

--- TCC/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $dbHost = 'Localhost:3307';
-    $dbUsername = 'root';
-    $dbPassword = 'etecjau';
-    $dbName = 'greenlifebd2';
-
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-?>
